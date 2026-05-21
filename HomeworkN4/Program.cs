ab96e41e6ceeaee2d94607b7f3809de820c86dcc
Giviko15 <vanishviligiviko@gmail.com>
2026-05-21 18:21:35 +0400

    homework_4

---
+++ HomeworkN4/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Givi Vanishvili");
+Console.WriteLine("Say Something:");
+string something = Console.ReadLine();
+Console.WriteLine(something);
