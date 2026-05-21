c9719f96980b30eda850a84a629f9f6439e47ab8
Giviko15 <vanishviligiviko@gmail.com>
2026-05-21 18:56:56 +0400

    homework4

--- HomeworkN4/obj/Debug/net10.0/HomeworkN4.AssemblyInfo.cs
+++ HomeworkN4/obj/Debug/net10.0/HomeworkN4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeworkN4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab96e41e6ceeaee2d94607b7f3809de820c86dcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeworkN4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeworkN4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
